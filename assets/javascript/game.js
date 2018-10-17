43fe5e0900e2b3cbb78173a44a6c470c55b648bc
wonder woman <rdelquiam@gmail.com>
2018-10-16 18:42:26 -0700

    <started js>

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var randomLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wins=0;
+var loses=0;
+var guessesLeft=10;
+var guesses=[];
+
+var luckyLetter= randomLetter[Math.floor(Math.random()*randomLetter.length)];
+console.log(luckyLetter);
+
+
+//have player choose a random letter
+//have 10 guesses 
+
