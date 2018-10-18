357db1b99d470d70888bde26fb2782e51c3254d0
wonder woman <rdelquiam@gmail.com>
2018-10-18 12:43:34 -0700

    <started js>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,57 @@
-var randomLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var wins=0;
-var loses=0;
-var guessesLeft=10;
-var guesses=[];
+//How to obtain user input?
+//Get user input using keypress
+document.onkeyup = (function (event) {
+    //Create variables
+    var randomLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 10;
+    var userGuesses = [];
 
-var luckyLetter= randomLetter[Math.floor(Math.random()*randomLetter.length)];
-console.log(luckyLetter);
+    var userGuesses = event.key;
+
+    //Have computer randomly choose a letter for player to guess
+
+    var luckyLetter = randomLetter[Math.floor(Math.random() * randomLetter.length)];
+    console.log(luckyLetter);
+
+    //have player choose a random letter
+    //have 10 guesses 
+    //compare userGuesses to random letter and determine results using conditional statements
+
+    //do i use a loop?
+    if (userGuesses === randomLetter) {
+        wins++;
+    } else {
+        losses++;
+    }
+
+    //track guessesLeft??
+    //store results in counter variables
+    //display results to the browser solution.  create a var to store a string interspersed with datea from results.  display the data using queryselector.
+
+    document.querySelector("guess").innerHTML=html;
+
+    var html=
+    "<p> wins:  " + wins + " </p>" +
+    "<p> wins:  " + losses + " </p>" +;
+
+}
 
 
-//have player choose a random letter
-//have 10 guesses 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
